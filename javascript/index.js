cf7232abcf086200e2be525712c1b992fa4dbb5e
nydia-subur <165659692+nydia-subur@users.noreply.github.com>
2024-04-08 00:47:43 +0700

    added JS function to update date/time for single location
    added updateTime() function to update date, and time every second  depending on the city

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
-//alert("h1");
+//singapore
+function updateTime() {
+  let singaporeDateElement = document.querySelector("#singapore p");
+  singaporeDateElement.innerHTML = moment().format("MMMM Do YYYY");
+
+  let singaporeTime = moment().tz("Asia/Singapore");
+  let singaporeTimeElement = document.querySelector("#singapore .city-time");
+  singaporeTimeElement.innerHTML = `${singaporeTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  )}`;
+
+  //saigon
+
+  let saigonDateElement = document.querySelector("#saigon p");
+  saigonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+
+  let saigonTime = moment().tz("Asia/saigon");
+  let saigonTimeElement = document.querySelector("#saigon .city-time");
+  saigonTimeElement.innerHTML = `${saigonTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  )}`;
+}
+updateTime();
+setInterval(updateTime, 1000);
